6616ddc40d733b80c922739f5eb6669ae3a86a96
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 15:39:39 +0530

    Add: Creating multiple Doublet and storing winning percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,9 +2,17 @@
 
 isHead=0
 isTail=1
+isHH=00
+isTT=11
+isHT=01
+isTH=10
 
 headCounter=0
 tailCounter=0
+hhCounter=0
+ttCounter=0
+htCounter=0
+thCounter=0
 
 declare -A result_dictonary
 
@@ -36,4 +44,51 @@ tailPercentage=${tailPercentage%.*}
 
 winningPercentage_dictonary["Head"]="$headPercentage"
 winningPercentage_dictonary["Tail"]="$tailPercentage"
+
+for((i=1; i<=50; i++))
+do
+        firstCoin=$(($RANDOM%2))
+        secondCoin=$(($RANDOM%2))
+        doubletCombination=$firstCoin$secondCoin
+        totalCombination=$i
+
+        if [ $doubletCombination -eq $isHH ]
+        then
+                ((hhCounter++))
+                hhCounter=$hhCounter
+                result_dictonary["HH"]="$hhCounter"
+        elif [ $doubletCombination -eq $isTT ]
+        then
+                ((ttCounter++))
+                ttCounter=$ttCounter
+                result_dictonary["TT"]="$ttCounter"
+        elif [ $doubletCombination -eq $isHT ]
+        then
+                ((htCounter++))
+                htCounter=$htCounter
+                result_dictonary["HT"]="$htCounter"
+        elif [ $doubletCombination -eq $isTH ]
+        then
+                ((thCounter++))
+                thCounter=$thCounter
+                result_dictonary["TH"]="$thCounter"
+        fi
+done
+
+hhPercentage=`awk "BEGIN {print ($hhCounter / $totalCombination) * 100 }"`
+hhPercentage=${hhPercentage%.*}
+
+ttPercentage=`awk "BEGIN {print ($ttCounter / $totalCombination) * 100 }"`
+ttPercentage=${ttPercentage%.*}
+
+htPercentage=`awk "BEGIN {print ($htCounter / $totalCombination) * 100 }"`
+htPercentage=${htPercentage%.*}
+
+thPercentage=`awk "BEGIN {print ($thCounter / $totalCombination) * 100 }"`
+thPercentage=${thPercentage%.*}
+
+winningPercentage_dictonary["HH"]="$hhPercentage"
+winningPercentage_dictonary["TT"]="$ttPercentage"
+winningPercentage_dictonary["HT"]="$htPercentage"
+winningPercentage_dictonary["TH"]="$thPercentage"
 
