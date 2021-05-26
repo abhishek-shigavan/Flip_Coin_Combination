87395f1b04d8f5c75c556f5f3c5d2523ae55d49c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 15:29:37 +0530

    Add: Creating multiple Singlet and storing winning percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,12 +3,37 @@
 isHead=0
 isTail=1
 
-coinSimulator=$(($RANDOM%2))
+headCounter=0
+tailCounter=0
 
-if [ $coinSimulator -eq $isHead ]
-then
-        echo " Head is the Winner "
-else
-        echo " Tail is the Winner "
-fi
+declare -A result_dictonary
+
+declare -A winningPercentage_dictonary
+
+for((i=1; i<=50; i++))
+do
+
+        coinSimulator=$(($RANDOM%2))
+        totalCount=$i
+
+        if [ $coinSimulator -eq $isHead ]
+        then
+                ((headCounter++))
+                headCounter=$headCounter
+                result_dictonary["Head"]="$headCounter"
+        else
+                ((tailCounter++))
+                tailCounter=$tailCounter
+                result_dictonary["Tail"]="$tailCounter"
+        fi
+done
+
+headPercentage=`awk "BEGIN {print ($headCounter / $totalCount) * 100 }"`
+headPercentage=${headPercentage%.*}
+
+tailPercentage=`awk "BEGIN {print ($tailCounter / $totalCount) * 100 }"`
+tailPercentage=${tailPercentage%.*}
+
+winningPercentage_dictonary["Head"]="$headPercentage"
+winningPercentage_dictonary["Tail"]="$tailPercentage"
 
